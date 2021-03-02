9a5f8060ca002e542eb07f8660f49799b9d3b765
Alice (AlexandrovMM) <a89261608027a@mail.ru>
2021-03-02 15:28:27 +0300

    code: вывод деление

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main() {
 	cin >> a >> b;
 	cout << "A + B = " << a + b << '\n'
 		<< "A - B = " << a - b << '\n'
-		<< "A * B = " << a * b << '\n';
+		<< "A * B = " << a * b << '\n'
+		<< "A / B = " << a / b << '\n';
 	return 0;
 }
