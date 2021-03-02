66d520ab45bb1d4ee1dd41559aaa1e001c2679e9
Alice (AlexandrovMM) <a89261608027a@mail.ru>
2021-03-02 15:41:01 +0300

    code: вывод максимума

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main() {
 	cout << "A + B = " << a + b << '\n'
 		<< "A - B = " << a - b << '\n'
 		<< "A * B = " << a * b << '\n'
-		<< "A / B = " << a / b << '\n';
+		<< "A / B = " << a / b << '\n'
+		<<"Max = " << max(a, b) << '\n';
+
 	return 0;
 }
